a206690420a5d511b7dca9051310ecf4e75e04d5
Agata-Lasek <129861701+Agata-Lasek@users.noreply.github.com>
2024-06-05 02:20:09 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,45 @@
-
 """
 Przykładowe funkcje matematyczne.
 """
-def add ( a: int , b: int) -> int:
+
+def add(a: int, b: int) -> int:
+    """
+    Dodaje dwie liczby.
+
+    :param a: Pierwsza liczba.
+    :param b: Druga liczba.
+    :return: Wynik dodawania.
+    """
     return a + b
-def subtract ( a: int , b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """
+    Odejmuje jedną liczbę od drugiej.
+
+    :param a: Pierwsza liczba.
+    :param b: Druga liczba.
+    :return: Wynik odejmowania.
+    """
     return a - b
-def multiply ( a: int , b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """
+    Mnoży dwie liczby.
+
+    :param a: Pierwsza liczba.
+    :param b: Druga liczba.
+    :return: Wynik mnożenia.
+    """
     return a * b
-def divide ( a: int , b: int) -> float :
+
+def divide(a: int, b: int) -> float:
+    """
+    Dzieli jedną liczbę przez drugą.
+
+    :param a: Liczba dzielona.
+    :param b: Liczba dzieląca.
+    :return: Wynik dzielenia.
+    """
     return a / b
-    
+
+#
